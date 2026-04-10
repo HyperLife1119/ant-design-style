f59c5eb9a6bc0771d1c5a062917393cec10aa299
HyperLife1119 <hyperlife1119@qq.com>
2026-04-10 16:55:12 +0800

    refactor: gen cmp design token

--- scripts/token.ts
+++ scripts/token.ts
@@ -1,7 +1,12 @@
 import { theme } from 'antd';
 
 const { initComponentToken } = await import('antd/es/input/style/token');
-console.log(initComponentToken(theme.getDesignToken({
+const { prepareComponentToken } = await import('antd/es/button/style/token');
+
+const designToken = theme.getDesignToken({
   token: { colorPrimary: '#eb2f96', },
   algorithm: theme.defaultAlgorithm
-})));
+})
+
+console.log(initComponentToken(designToken));
+console.log(prepareComponentToken(designToken));
